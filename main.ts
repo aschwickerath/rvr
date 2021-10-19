2f529072a64e8a62a35b7fe2c483be7dcd26cd07
aschwickerath <92823707+aschwickerath@users.noreply.github.com>
2021-10-19 14:48:33 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+sphero.resetYaw()
+let buttonA = input.buttonIsPressed(Button.A)
+let buttonB = input.buttonIsPressed(Button.B)
+let buttonAB = input.buttonIsPressed(Button.AB)
+if (buttonA) {
+    sphero.drive(0, 0)
+} else if (buttonB) {
+    sphero.drive(0, 0)
+} else if (buttonAB) {
+    sphero.drive(0, 0)
+} else {
+    sphero.drive(0, 0)
+}
